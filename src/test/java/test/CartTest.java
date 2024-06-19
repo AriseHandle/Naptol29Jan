7bf4bb3d224be3ac59f54e9f5fa49f5e2c19b263
Subodh Patil <subodhpatil69@gmail.com>
2024-06-19 21:03:26 +0530

    New Test cases for cart feature added

---
+++ src/test/java/test/CartTest.java
@@ -0,0 +1,43 @@
+package test;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import pojo.Browser;
+import pom.CartPage;
+import pom.NaptolHomePage;
+import pom.ProductDetailPage;
+import pom.ProductResultPage;
+
+public class CartTest extends BaseTest {
+	
+	@BeforeMethod
+	public void openApplication() {
+		driver =Browser.launchApplication();
+	}
+	
+	@Test
+	public void verifyAddToCartFromOrderDeatils()
+	{
+		naptolHomePage =new NaptolHomePage(driver);
+		naptolHomePage.enterProductToSearch("cooker");
+		naptolHomePage.clickOnSearch();
+		productResultPage = new ProductResultPage(driver);
+		productResultPage.selectDesiredProduct(0);
+		switchToChildBrowser();
+		productDetailPage = new ProductDetailPage(driver);
+		String ExpectedName = productDetailPage.getProductName();
+		String ExpectedPrice = productDetailPage.getProductPrice();
+		String ExpectedShippingPrice = productDetailPage.getShippingPrice();
+		productDetailPage.clickOnBuyButton();
+		cartPage =new CartPage(driver);
+		Assert.assertEquals(cartPage.getNumberOfProductsInCart(), 1);
+		Assert.assertEquals(cartPage.getProductName(0), ExpectedName);
+		Assert.assertEquals(cartPage.getProductPrice(1), ExpectedPrice);
+		Assert.assertEquals(cartPage.getShippingPrice(1), ExpectedShippingPrice);	
+	}
+}
